455cbb88f35c478d9082f82800729603f2f3ec18
Brad Lindsay <bslindsa@gmail.com>
2022-01-24 10:57:41 -0700

    Complete category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -18,7 +18,7 @@ router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
-    const categoryData = await Category.findByPk(req.params.id. {
+    const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }]
     });
     if (!categoryData) {
@@ -62,17 +62,13 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try {
-    const categoryData = await Location.destroy({
-      where: {
-        id: req.params.id
-      }
+    const categoryData = await Category.destroy({
+      where: { id: req.params.id }
     });
-
     if (!categoryData) {
       res.status(404).json({ message: 'No category found with this id!' });
       return;
     }
-
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
